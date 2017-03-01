55b4f9710439891cf49d9e2fdb8cc1207b304324
Crac <MOCrac@romania.luxoft.com>
2017-03-01 17:57:33 +0200

    modify package

--- spittr/src/main/java/spittr/web/HomeController.java
+++ spittr/src/main/java/spittr/web/HomeController.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 @Controller
 public class HomeController {
-	@RequestMapping(value = "/", method = GET)
+	
+	@RequestMapping(value = "/", method=GET)
 	public String home() {
 		return "home";
 	}
